e9c4486db41683356163c48f982e86fc49588342
markcharyk <markcharyk@gmail.com>
2014-05-20 09:11:52 -0700

    Move conditions into class namespace

--- game/models.py
+++ game/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-conditions = (
-    (0,3,6),
-    (1,4,7),
-    (2,5,8),
-    (0,1,2),
-    (3,4,5),
-    (6,7,8),
-    (0,4,8),
-    (2,4,6),)
 
 
 class Board(models.Model):
     """For displaying and keeping track of the state of the game"""
     spaces = models.CharField(max_length=9, default = u'         ')
+    conditions = (
+        (0,3,6),
+        (1,4,7),
+        (2,5,8),
+        (0,1,2),
+        (3,4,5),
+        (6,7,8),
+        (0,4,8),
+        (2,4,6),)
 
     def __repr__(self):
         result = ''
@@ -24,7 +24,7 @@ class Board(models.Model):
 
     def check_for_end(self):
         """Check to see if a game is won or tied"""
-        for run in conditions:
+        for run in self.conditions:
             if self.spaces[run[0]] == ' ':
                 continue
             if self.spaces[run[0]] == self.spaces[run[1]] and self.spaces[run[1]] == self.spaces[run[2]]:
